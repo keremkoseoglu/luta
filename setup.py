80e336ad122a6f2efb4d461acd5ddf71b200cff6
Dr. Kerem Koseoglu <kerem@keremkoseoglu.com>
2021-04-10 12:43:27 +0300

    setup fix

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires="3.8.2",
+    python_requires=">=3.6",
     install_requires=[],
     include_package_data=True
 )
