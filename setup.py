b05143e6d203f59b57ad3759f46fbafe6ef13cad
Dr. Kerem Koseoglu <kerem@keremkoseoglu.com>
2021-04-10 12:53:06 +0300

    selenium

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@ import setuptools
 
 setuptools.setup(
     name="luta-keremkoseoglu",
-    version="0.0.1",
+    version="0.0.2",
     author="Kerem Koseoglu",
     author_email="kerem@keremkoseoglu.com",
     description="Web spider",
     long_description="Seleium based web spider",
     long_description_content_type="text/markdown",
     url="https://github.com/keremkoseoglu/luta",
-    packages=setuptools.find_packages(),
+    packages=["selenium"],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
